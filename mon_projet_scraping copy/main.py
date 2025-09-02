ce333c04aef310c736d2b2d715c911ed1b39ded9
Chaima425 <chaimaabdraouf@gmail.com>
2025-09-02 14:50:53 +0200

    Delete mon_projet_scraping copy directory

--- mon_projet_scraping copy/main.py
+++
@@ -1,7 +0,0 @@
-# fichier  lance le pipeline
-from pipelines.pipeline_scraping import run_scraping_pipeline
-
-if __name__ == "__main__":
-    print("Début du script main.py")
-    run_scraping_pipeline()
-    print("Fin du script main.py")
